b79fcb8dfe4904e31903e1d857c9ac9515d16890
Chiranjeevi <81378515+ChiranjeeviRajendra@users.noreply.github.com>
2026-09-25 08:26:01 +0530

    Feature/foundation code one (#1)
    * Folder structure created
    
    * readme file format
    
    ---------
    
    Co-authored-by: Chiranjeevi <chiru234@gmail.com>

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'clinician',
+    version= '0.0.0',
+    author= 'Chiranjeevi Rajendra',
+    packages= find_packages(),
+    install_requires = []
+)
